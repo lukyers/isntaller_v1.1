0c936ecb62bf9c5fbc7fa4ea01afbebefa8fa76c
Richard <Richard.Zhang@emc.com>
2015-08-10 12:26:51 +0800

    v1.3 Functional

--- Install.cmd
+++ Install.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
+%windir%\system32\windowspowershell\v1.0\powershell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
 PAUSE
